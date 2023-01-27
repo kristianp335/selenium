7797a940b1cfb456c788508223ca676cc5e9c247
Olaf Kock <olaf.kock+github@liferay.com>
2023-01-27 15:10:21 +0100

    login failed occasionally. Wait a bit longer

--- src/com/liferay/sales/selenium/liferaycity/LiferayCityBaseClickpath.java
+++ src/com/liferay/sales/selenium/liferaycity/LiferayCityBaseClickpath.java
@@ -13,7 +13,8 @@ public abstract class LiferayCityBaseClickpath extends ClickpathBase {
 
 	protected void doLogin(String username, String password) {
 	    doGoTo(baseUrl + "welcome?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_com_liferay_login_web_portlet_LoginPortlet_mvcRenderCommandName=%2Flogin%2Flogin&saveLastPath=false");
-	    
+
+	    sleep(500);
 	    getLoginField("login").sendKeys(Keys.chord(Keys.CONTROL, "a"));;
 		getLoginField("login").sendKeys(Keys.DELETE);
 		getLoginField("login").sendKeys(username);
@@ -21,6 +22,8 @@ public abstract class LiferayCityBaseClickpath extends ClickpathBase {
 
 		getLoginField("password").sendKeys(password); 
 		sleep(500);
+		
+		log("INFO (doLogin): Logging in as "+ username);
 		
 		getLoginField("login").sendKeys(Keys.ENTER);
 	    sleep(8000);	    
