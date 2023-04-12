33467f95fe2fce37b36bc30ea8a515c98f0d706e
Olaf Kock <olaf.kock+github@liferay.com>
2023-04-12 09:52:03 +0200

    multilingual navigation

--- src/com/liferay/sales/selenium/insurance/LiferayInsuranceBaseClickpath.java
+++ src/com/liferay/sales/selenium/insurance/LiferayInsuranceBaseClickpath.java
@@ -5,6 +5,7 @@ import com.liferay.sales.selenium.api.DriverInitializer;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -47,25 +48,60 @@ public abstract class LiferayInsuranceBaseClickpath extends ClickpathBase {
 
 	/**
 	 * Mouseover trickery to navigate to menu items
-	 * (too lazy to make it more elegant with appropriate xpath - at least for now)
-	 * @param level1
-	 * @param level2
+	 * @param level1 menu level 1
+	 * @param level2 can be null, to navigate to level 1 menu item
 	 */
 	public void navigateTo(String level1, String level2) {
 		WebDriver driver = getDriver();
 		Actions builder = new Actions(driver);
-//		WebElement element = driver.findElement(By.linkText(level1));
-		WebElement l1element = driver.findElement(By.xpath("//div[@id='navbarmain']//a[text()[contains(., '"+level1+"')]]"));
+		WebElement l1element = driver.findElement(By.xpath("//div[@id='navbarmain']//a[normalize-space()='"+level1+"']"));
 		builder.moveToElement(l1element).build().perform();
 		sleep(1000);
 		if(level2 == null) {
 			l1element.click();
 		} else {
-			WebElement l2element = driver.findElement(By.xpath("//div[@id='navbarmain']/ul/li/ul/li/a[text()[contains(., '"+level2+"')]]"));
+			WebElement l2element = driver.findElement(By.xpath("//div[@id='navbarmain']/ul/li/ul/li/a[normalize-space()='"+level2+"']"));
 			l2element.click();
-			//doClick(l2element);
 		}
 		sleep(defaultSleep);
+	}
+
+	/**
+	 * Navigate to menu item l1/l2 (or just l1 if l2==null). The array provides
+	 * different language versions of the menus. 
+	 * @param level1
+	 * @param level2
+	 */
+	protected void navigateTo(String[] level1, String[] level2) {
+		WebDriver driver = getDriver();
+		Actions builder = new Actions(driver);
+		WebElement l1element = null;
+		for (int i = 0; i < level1.length; i++) {
+			try {
+				l1element = driver.findElement(By.xpath("//div[@id='navbarmain']//a[normalize-space()='"+level1[i]+"']"));
+			} catch (NoSuchElementException ignore) {
+			}
+			System.out.println("Navigation " + level1[i] + ": " + (l1element==null? "not ":"") + "found");
+			if(l1element != null) break;
+		}
+		builder.moveToElement(l1element).build().perform();
+		sleep(1000);
+		if(level2 == null) {
+			l1element.click();
+		} else {
+			WebElement l2element = null;
+			for (int i = 0; i < level2.length; i++) {
+				try {
+					System.out.println("Searching for L2 " + level2[i]);
+					l2element = driver.findElement(By.xpath("//div[@id='navbarmain']/ul/li/ul/li/a[normalize-space()='"+level2[i]+"']"));
+				} catch (NoSuchElementException ignore) {
+				}
+				if(l2element!=null) break;
+			}
+			l2element.click();
+		}
+		sleep(defaultSleep);
+		
 	}
 	
 	public void selectLanguage(String language) {
@@ -74,5 +110,4 @@ public abstract class LiferayInsuranceBaseClickpath extends ClickpathBase {
 		select.selectByVisibleText(language);
 		sleep(defaultSleep);
 	}
-	
 }
--- src/com/liferay/sales/selenium/insurance/LiferayInsuranceClickpath1.java
+++ src/com/liferay/sales/selenium/insurance/LiferayInsuranceClickpath1.java
@@ -2,21 +2,81 @@ package com.liferay.sales.selenium.insurance;
 
 import com.liferay.sales.selenium.api.DriverInitializer;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+
 public class LiferayInsuranceClickpath1 extends LiferayInsuranceBaseClickpath {
 
+	private String[] MENU1_HOME = new String[] {"Home", "Start"};
+	private String[] MENU1_INSURANCE_PLANS = new String[] {"Insurance Plans", "Versicherungen"};
+	private String[] MENU2_IP_MOBILE_INSURANCE = new String[] {"Mobile Insurance", "Handyversicherung"};
+	private String[] MENU1_CONTACT_US = new String[] {"Contact us", "Kontakt"};
+	
 	public LiferayInsuranceClickpath1(DriverInitializer di, String baseUrl) {
 		super(di, baseUrl);
 	}
-
 	
 	@Override
 	public void run(String username, String password) {
 		doGoTo(baseUrl);
-		navigateTo("Insurance Plans", "Mobile Insurance");
-		navigateTo("Contact us", null);
 		selectLanguage("deutsch");
-		selectLanguage("english");
-		selectLanguage("العربية");
+//		navigateTo("Insurance Plans", "Mobile Insurance");
+		navigateTo(MENU1_INSURANCE_PLANS, MENU2_IP_MOBILE_INSURANCE);
+		navigateTo(MENU1_HOME, null);
+		navigateTo(MENU1_CONTACT_US, null);
+//		selectLanguage("english");
+		fillOutContactForm(42);
 	}
 
+	private void fillOutContactForm(int hint) {
+		List<WebElement> textInputs = getElementsByXPath("//form[@id='_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_INSTANCE_xlzy_fm']//input[@type='text']");
+	    List<WebElement> radioInputs = getElementsByXPath("//form[@id='_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_INSTANCE_xlzy_fm']//input[@type='radio']");
+	    List<WebElement> textareas = getElementsByXPath("//form[@id='_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_INSTANCE_xlzy_fm']//textarea");
+	    List<WebElement> selectInputs = getElementsByXPath("//form[@id='_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_INSTANCE_xlzy_fm']//div[contains(@class,'form-builder-select-field')]");
+	    List<WebElement> submits = getElementsByXPath("//form[@id='_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_INSTANCE_xlzy_fm']//button[@type='submit']");
+	    System.out.println("Found Inputs: ");
+	    System.out.println("  TextInput: " + textInputs.size());
+	    System.out.println("  RadioInput: " + radioInputs.size());
+	    System.out.println("  Selects: " + selectInputs.size());
+	    System.out.println("  Textareas: " + textareas.size());
+	    System.out.println("  Submits: " + submits.size());
+	    WebElement name = textInputs.get(0);
+	    WebElement surname = textInputs.get(1);
+	    WebElement telephone = textInputs.get(2);
+	    WebElement mail = textInputs.get(3);
+	    WebElement policy = textInputs.get(4);
+	    WebElement choices = selectInputs.get(0);
+
+	    WebElement message = textareas.get(0);
+	    WebElement submit = submits.get(0);
+	    
+	    scrollTo(name);
+	    name.sendKeys("Bloggs (" + hint + ")");
+	    sleep(100);
+	    surname.sendKeys("Joe");
+	    sleep(100);
+	    telephone.sendKeys(""+hint+"-1234567");
+	    sleep(100);
+	    mail.sendKeys("test+" +hint + "@liferay.com");
+	    sleep(100);
+	    policy.sendKeys(""+hint+"-"+hint);
+	    sleep(100);
+	    choices.click();
+	    sleep(1000);
+
+	    int chosenItem = (int) (Math.random()*4+1); 
+	    WebElement choice = getElementByXPath("//button[@data-testid='dropdownItem-"+chosenItem+"']");
+	    choice.click();
+	    sleep(500);
+
+	    scrollTo(message);
+	    message.sendKeys("This is a random comment with hint " + hint);
+	    sleep(500);
+	    if(Math.random() < 0.9 ) {
+	    	doClick(submit);
+	    } else {
+	    	System.out.println("Abandoning Form, it's filled out but not submitted");
+	    }
+	}
 }
